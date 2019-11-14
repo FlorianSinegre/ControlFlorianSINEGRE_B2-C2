1ae73c9b50e07d7b5b10609d026681d373a1de3d
FlorianSinegre <45358364+FlorianSinegre@users.noreply.github.com>
2019-11-14 11:15:29 +0100

    Ajoutez des fichiers projet.

---
+++ EvalMapppingFlorianSinegre.ORM/Migrations/201911141014513_hop.cs
@@ -0,0 +1,16 @@
+﻿namespace EvalMapppingFlorianSinegre.ORM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class hop : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ EvalMapppingFlorianSinegre.ORM/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace EvalMapppingFlorianSinegre.ORM.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EvalMapppingFlorianSinegre.ORM.ContexteBDD>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EvalMapppingFlorianSinegre.ORM.ContexteBDD context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
